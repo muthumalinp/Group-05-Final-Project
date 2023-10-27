4dcc4e8af9ed6d606d080e2cf6ddc661a26b13c3
Yudeshika Kaushalya <yudeshika1998@gmail.com>
2023-10-27 10:52:56 +0530

    update productcss

---
+++ resources/views/project/public/producthairt.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css\product\productform.css">
+   
+    <title>Product Hair Straightening Form</title>
+</head>
+<body>
+    <div class="mainFrame">
+
+        <div class="topMain">
+
+            <div class="topMainHead">
+                <h1>Order your Products</h1>
+            </div>
+
+            <div class="topMainTail clearfix">
+                <div class="HeadLink_Left">
+                    <ul>
+                        <li><a href="/Product/HairStraghtening">Hair Straightening</a></li>
+                        <li><a href="/Product/HairTreatment">Hair Treatment</a></li>
+                    </ul>
+                </div>
+                <div class="HeadLink_Left">
+                    <ul>
+                        <li><a href="/Product/HairColoring&Highliting">Hair coloring and Highlighting</a></li>
+                        <li><a href="/Product/Facial&CleanUp">Facial and Cleanup</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="Horizontal_line1"></div>
+
+        </div><!--topMain-->
+    </div>
+    
+</body>
+</html>
