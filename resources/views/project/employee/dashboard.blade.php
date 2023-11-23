382cd44b33f8226f7ae82eaf32cccd3c69831b4c
AshanAnjana <ashangunasekara5@gmail.com>
2023-11-23 09:22:24 +0530

    employee dashboard

--- resources/views/project/employee/dashboard.blade.php
+++ resources/views/project/employee/dashboard.blade.php
@@ -3,9 +3,66 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="public\css\employ\employeeHomeStyle.css">
+    
     <title>Employee</title>
 </head>
 <body>
-    <h1 style="text-align: center;">This is the Employee Dashboard</h1>
+    <div class="Full-Main">
+        <div class="Full1 clearfix">
+                <div class="tailButn">
+                    <button type="button" class="head-but">Home</button>
+                    <button type="button" class="head-but">Work</button>
+                </div>
+                <div class="site-search">
+                    <form method="get" action="employeeHome.html">
+                        <input type="search" name="search-box">
+                        <button type="submit"></button>
+                    </form>
+                </div>
+        </div>
+
+        <div class="p-tag">
+                <p>
+                    Keep your dreams alive. Understand to achieve anything requires faith and belief in yourself, vision,
+                    hard work, determination, and dedication.Remember all things are possible for those who believe.
+                </p>
+        </div>
+
+        <div class="Full2">
+            <div class="Full21-Left">
+                <img src="public\css\employ\img\Attendance_1.jpg">
+            </div>
+            <div class="Full21-Right clearfix">
+                <div class="Full22-Head clearfix">
+                    <div class="work">
+                        <h3>Hi ANN,<br> Good Morning,<p>Have a nice day</p></h3>
+                    </div>
+                    <div class="leave">
+                        <button type="button">QUICK STATES</button>
+                    </div>
+                </div>
+                <div class="Full22-Tail">
+                    <div class="boxes">
+                        <div class="box"><a href="#">WORK</a></div>
+                        <div class="box"><a href="#">LEAVE</a></div>
+                        <div class="box"><a href="#">HOLIDAY</a></div>
+                        <div class="box"><a href="#">MEETING</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="Full3 clearfix">
+            <div class="Full3-Left">
+                <img src="public\css\employ\img\rectangle_12.png">
+            </div>
+            <div class="Full3-Right">
+                <img src="public\css\employ\img\Attendance_1.jpg">
+            </div>
+        </div>
+        
+    </div>
 </body>
 </html>
