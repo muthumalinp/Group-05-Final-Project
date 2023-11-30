5ffd0b5270946fd03eee5ccc87ad744da9fa26ea
muthumali <muthumalinp@gmail.com>
2023-11-30 17:13:20 +0530

    employee register mail(but not working)

---
+++ resources/views/emails/employee-registered.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Email</title>
+</head>
+<body>
+    <p>Dear {{$name}},</p><br><br>
+    <p>Welcome to Our Team!</p><br><br>
+    <p>Owner-Salon Yaraa</p>
+</body>
+</html>
