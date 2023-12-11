77738bf490e281a1ca843053ba2ee7024e9a5396
Manuji Siriwardhana <manujisiriwardana@gmail.com>
2023-12-11 18:14:52 +0530

    Update Booking

--- resources/views/project/customer/booking.blade.php
+++ resources/views/project/customer/booking.blade.php
@@ -9,122 +9,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Booking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .body1 {
-            padding-top: 200px;
-        }
-        .btn-custom:active, .btn-custom:focus {
-            background-color: #fff;
-            border-color: #ffff;
-            color: black;
-            width: 200px;
-            text-align: left;
-        }
-        .hidden-content {
-            display: none;
-        }
-    </style>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.9/dist/flatpickr.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr@4.6.9/dist/flatpickr.min.css">
 </head>
-<body class="body1">
-    <div class="container">
-        <form action="{{ route('bookings.store') }}" method="POST">
-            @csrf
-            <div class="row contentToHide">
-                <div class="col-md-3">
-                    <div class="col-12">
-                        @if(isset($serviceCategories))
-                        @foreach($serviceCategories as $serviceCategory)
-    @if(is_object($serviceCategory) && property_exists($serviceCategory, 'id'))
-        <button type="button" class="btn btn-custom" onclick="toggleContent('{{ $serviceCategory->id }}')">
-            {{ $serviceCategory->name }}
-        </button><br/>
-    @else
-        <p>Invalid service category data:</p>
-        <pre>{{ var_dump($serviceCategory) }}</pre>
-    @endif
-@endforeach
 
-                        @else
-                            <p>No service categories found.</p>
-                        @endif
-                    </div>
-                </div>
+<body>
+<div style="padding-top: 100px">
+<form action="" method="post">
 
-                <div class="col-md-6">
-                    <!-- Container to display services -->
-                    <div id="servicesContainer" class="hidden-content">  
-                        <div class="row contentToHide">
-                            <div class="col-md-3">
-                                <div class="col-12">
-                                    @foreach($services as $service)
-                                        <button type="button" class="btn btn-custom" onclick="toggleContent('{{ $service->id }}')">
-                                            {{ $service->service_name }}
-                                        </button>      
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-    </div>
+<label for="serviceCtaegory">Select a Service Category:</label>
+  <select id="serviceCtaegory" name="serviceCtaegory">
+  @foreach($HairServices as $HairService)
+            <option value="{{ $HairService }}">{{ $HairService }}</option>
+        @endforeach
+</select>
+  <br><br>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        function toggleContent(categoryId) {
-            var allContent = document.querySelectorAll('.hidden-content');
-            allContent.forEach(function(content) {
-                content.style.display = 'none';
-            });
+  <label for="service">Select a Service:</label>
+  <select id="service" name="service">
+        @foreach($HairCuts as $HairCut)
+            <option value="{{ $HairCut }}">{{ $HairCut }}</option>
+        @endforeach
+    </select>
+  <br><br>
 
-            // Show the services container
-            const servicesContainer = document.getElementById('servicesContainer');
-            servicesContainer.style.display = 'block';
+  <label for="stylist">Select a Stylist:</label>
+    <select id="stylist" name="stylist">
+        @foreach($Hairstylists as $Hairstylist)
+            <option value="{{ $Hairstylist }}">{{ $Hairstylist }}</option>
+        @endforeach
+    </select>
+    <br><br>
 
-            // Load services when a category is clicked
-            loadServices(categoryId);
-        }
+    <label for="datePicker">SELECT DATE AND TIME:</label>
+            <input type="text" id="datePicker" name="bookingDate" readonly>
+            <br><br>
+    
+    
 
-        function loadServices(categoryId) {
-            // Make an asynchronous request to fetch services for the selected category
-            // Replace the URL with the actual endpoint to fetch services
-            fetch(`/api/services/${categoryId}`)
-                .then(response => response.json())
-                .then(services => {
-                    // Update the services container with the fetched services
-                    const servicesContainer = document.getElementById('servicesContainer');
-                    servicesContainer.innerHTML = '';
 
-                    console.log(services);
-                    if (services.length > 0) {
-                        // Display each service
-                        services.forEach(service => {
-                            const serviceElement = document.createElement('div');
-                            serviceElement.textContent = service.service_name;
-                            // Attach a click event to display the selected service content
-                            serviceElement.addEventListener('click', function() {
-                                displaySelectedService(service.service_name);
-                            });
-                            servicesContainer.appendChild(serviceElement);
-                        });
-                    } else {
-                        // Display a message if no services are available
-                        servicesContainer.textContent = 'No services found for this category.';
-                    }
-                })
-                .catch(error => {
-                    console.error('Error fetching services:', error);
+  <input type="submit" value="Submit">
+</form>
+
+<script>
+            // Initialize Flatpickr on the date input field
+            document.addEventListener('DOMContentLoaded', function () {
+                var datePicker = document.getElementById('datePicker');
+
+                flatpickr(datePicker, {
+                    enable: [
+                        // Specify available dates or a range of dates
+                        '2023-01-01',
+                        '2023-01-10',
+                        { from: '2000-01-01', to: '2030-12-31' }
+                    ],
+                    dateFormat: 'Y-m-d', // Adjust the date format as needed
+                    // Additional options go here
                 });
-        }
+            });
+        </script>
 
-        function displaySelectedService(serviceName) {
-            const selectedServiceContent = document.getElementById('selectedServiceContent');
-            selectedServiceContent.innerHTML = `<h1>You Selected ${serviceName}</h1>`;
-            selectedServiceContent.style.display = 'block';
-        }
-    </script>
 </body>
 </html>
